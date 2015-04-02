345a4b648c4b99a9bb3c7c26bf98a6e4677e0418
loostro <loostro@gmail.com>
2015-04-02 22:57:16 +0200

    Update AssetsCompilerPass.php

--- DependencyInjection/Compiler/AssetsCompilerPass.php
+++ DependencyInjection/Compiler/AssetsCompilerPass.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Admingenerator\GeneratorBundle\DependencyInjection\Compiler;
+namespace Admingenerator\FormExtensionsBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
