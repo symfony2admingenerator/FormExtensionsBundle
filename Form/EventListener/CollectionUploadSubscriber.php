b313b3785f6118e311c5cbcbda5fc82345416cce
loostro <loostro@gmail.com>
2015-05-17 22:16:12 +0200

    Fix filter path null error

--- Form/EventListener/CollectionUploadSubscriber.php
+++ Form/EventListener/CollectionUploadSubscriber.php
@@ -218,8 +218,20 @@ class CollectionUploadSubscriber implements EventSubscriberInterface
         $form = $event->getForm();
         $data = $form->getParent()->getData();
 
+        $current = $form;
+        $isAllValid = true;
+
+        // check recuresively this form and all it's parents
+        do {
+            if (!$current->isValid()) {
+                $isAllValid = false;
+                break;
+            }
+            $current = $current->getParent();
+        } while (null !== $current);
+
         $getter = 'get'.ucfirst($this->propertyName);
-        if (!$form->isValid() && $data->$getter() instanceof ArrayCollection) {
+        if (!$isAllValid && $data->$getter() instanceof ArrayCollection) {
             // remove files absent in the original collection
             $data->$getter()->clear();
 
