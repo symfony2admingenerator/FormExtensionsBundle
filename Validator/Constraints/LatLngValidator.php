6be01fc0d3b9bc1ff715116695a069951f9f929e
Tobias Feijten <tobias93@live.nl>
2025-12-02 18:28:53 +0100

    Add compatibility with Symfony 7 and 8, rework service config to PHP

--- Validator/Constraints/LatLngValidator.php
+++ Validator/Constraints/LatLngValidator.php
@@ -8,19 +8,17 @@ use Symfony\Component\Validator\Exception\InvalidArgumentException;
 
 class LatLngValidator extends ConstraintValidator
 {
-    public function validate($value, Constraint $constraint): bool
+    public function validate($value, Constraint $constraint): void
     {
         if (!($constraint instanceof LatLng)) {
             throw new InvalidArgumentException(sprintf('Expected %s, got %s', LatLng::class, $constraint::class));
         }
         if (!preg_match('/^[0-9\-\.]+$/', $value['lat'], $matches) || !preg_match('/^[0-9\-\.]+$/', $value['lng'], $matches)) {
             $this->context->addViolation($constraint->message, ['%lat%' => (float)$value['lat'], '%lng%' => (float)$value['lng']]);
-            return false;
+            return;
         }
         if ($value['lat'] > 90 || $value['lat'] < -90 || $value['lng'] > 180 || $value['lng'] < -180) {
             $this->context->addViolation($constraint->message, ['%lat%' => (float)$value['lat'], '%lng%' => (float)$value['lng']]);
-            return false;
         }
-        return true;
     }
 }
